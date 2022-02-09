2796ad08ac99d9f994a5b0921bc7c8f215519574
rybaier <rybaier@gmail.com>
2022-02-09 08:35:12 -0800

    trip controller built for testing pre user authorization tokens

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -2,4 +2,68 @@ const express = require('express')
 const Trip = require('../models/Trip')
 const router = express.Router()
 
+const { requireToken } =require('../middleware/authorization')
+const res = require('express/lib/response')
+// add require token in after all route testing and  authorization built Stretch
 
+//get all trips
+router.get("/trips", async (req, res, next) => {
+    try{
+        const trip = await Trip.find({});
+        res.json(trip); 
+    } catch (err) {
+        next(err);
+    }
+});
+
+//Get one trip by id
+router.get("/trips:id", async (req, res, next) => {
+    try {
+        const oneTrip = await Trip.findById(req.params.id);
+        res.json(oneTrip);
+    } catch (err){
+        next(err);
+    }
+});
+
+//Add a new trip based on the body of the request
+router.post("/trips", async (req, res, next) => {
+    try {
+        const newTrip = await Trip.create(req.body);
+        res.status(201).json(newTrip);
+    } catch (err) {
+        next(err);
+    }
+});
+
+//Updating trip data, specify which one by ID
+router.put("/trips/:id", async (req, res, next) => {
+    try {
+        const tripToUpdate = await Trip.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        );
+        res.status(200).json(tripToUpdate);
+    } catch (err) {
+        next(err);
+    }
+});
+
+//Delete a trip, specify which one by ID
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const tripToDelete = await Trip.findByIdAndDelete(
+            req.params.id
+        );
+        if(tripToDelete) {
+            res.sendStatus(204); 
+        } else {
+            res.sendStatus(404);
+        }
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = router
