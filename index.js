14da6c60eec8513de2103101e24cc8f8b1c573cd
rybaier <rybaier@gmail.com>
2022-02-09 13:57:04 -0800

    push after testing complete

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ app.use(cors())
 //ROUTES
 //===========
 // //Redirect currently commented out believe main page on front end will be create trip 
-// app.get('/', (req, res) => {
-//      res.redirect('/trips')
-// })
+app.get('/', (req, res) => {
+     res.redirect('/trips')
+})
 
 //===========
 //CONTROLLERS
@@ -28,8 +28,8 @@ app.use(cors())
 const tripController = require('./controllers/tripController');
 app.use('/trips', tripController);
 
-// const userController = require('./controllers/userController');
-// app.use('/', userController);
+const userController = require('./controllers/userController');
+app.use('/', userController);
 
 //============
 //START SERVER
