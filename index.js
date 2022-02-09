f9b654e53792ee363ada22ac8e8c1d4fc07364a9
rybaier <rybaier@gmail.com>
2022-02-09 08:58:12 -0800

    index.js file built connection to server and database successful

---
+++ index.js
@@ -0,0 +1,45 @@
+//============
+//BASIC CONFIG
+//============
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('./db/connection');
+app.set('port', process.env.PORT || 8000);
+
+//==========
+//MIDDLEWARE
+//==========
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
+//===========
+//ROUTES
+//===========
+// //Redirect currently commented out believe main page on front end will be create trip 
+// app.get('/', (req, res) => {
+//      res.redirect('/trips/')
+// })
+
+//===========
+//CONTROLLERS
+//===========
+const tripController = require('./controllers/tripController');
+app.use('/trips/', tripController);
+
+const userController = require('./controllers/userController');
+app.use('/', userController);
+
+//============
+//START SERVER
+//============
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode || 500; //500 is internal server error
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).send(message);
+});
+
+app.listen(app.get('port'), () => {
+	console.log(`✅ PORT: ${app.get('port')} 🐲 🌟`);
+});
